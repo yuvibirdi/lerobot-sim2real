15d20303b5ea984c5064add59d724924dd7cfe75
Xander <xhinrichsen@ucsd.edu>
2025-03-24 17:23:17 -0700

    fixed overlay

--- lerobot_sim2real/scripts/camera_alignment.py
+++ lerobot_sim2real/scripts/camera_alignment.py
@@ -1,27 +1,24 @@
+import time
 from dataclasses import dataclass
 from typing import Optional
-import time
 
 import gymnasium as gym
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import tyro
-
-from mani_skill.utils.visualization.misc import tile_images
-from mani_skill.utils.wrappers.flatten import FlattenRGBDObservationWrapper
-from mani_skill.utils.wrappers.record import RecordEpisode
-from lerobot.common.robot_devices.cameras.configs import (
-    OpenCVCameraConfig,
-)
+from lerobot.common.robot_devices.cameras.configs import OpenCVCameraConfig
 from lerobot.common.robot_devices.motors.configs import DynamixelMotorsBusConfig
 from lerobot.common.robot_devices.robots.configs import KochRobotConfig
 from lerobot.common.robot_devices.robots.manipulator import ManipulatorRobot
 from mani_skill.agents.robots.lerobot.manipulator import LeRobotRealAgent
 from mani_skill.envs.sim2real_env import Sim2RealEnv
-
-from mani_skill.utils.structs.pose import Pose
 from mani_skill.utils import common, sapien_utils
+from mani_skill.utils.structs.pose import Pose
+from mani_skill.utils.visualization.misc import tile_images
+from mani_skill.utils.wrappers.flatten import FlattenRGBDObservationWrapper
+from mani_skill.utils.wrappers.record import RecordEpisode
+
 
 @dataclass
 class Args:
@@ -40,14 +37,13 @@ def overlay_envs(sim_env, real_env):
     assert sorted(real_obs.keys()) == sorted(
         sim_obs.keys()
     ), f"real camera names {real_obs.keys()} and sim camera names {sim_obs.keys()} differ"
-    green_screens = dict()
-    for name in real_obs:
-        green_screens[name] = real_obs[name]["rgb"]
 
     overlaid_dict = sim_env.get_obs()["sensor_data"]
     overlaid_imgs = []
     for name in overlaid_dict:
-        overlaid_imgs.append(overlaid_dict[name]["rgb"][0] / 255)
+        real_imgs = real_obs[name]["rgb"][0] / 255
+        sim_imgs = overlaid_dict[name]["rgb"][0].cpu() / 255
+        overlaid_imgs.append(0.5 * real_imgs + 0.5 * sim_imgs)
 
     return tile_images(overlaid_imgs)
 
@@ -60,57 +56,69 @@ MOVEMENT_SPEED = 0.1  # units per second
 FOV_CHANGE_SPEED = 0.1  # radians per second
 help_message_printed = False  # Flag to track if we've printed the help message
 
+
 def on_key_press(event):
     global active_keys
     active_keys.add(event.key)
 
+
 def on_key_release(event):
     global active_keys
     active_keys.discard(event.key)
+
 
 def update_camera(sim_env):
     global camera_offset, fov_offset, last_frame_time, help_message_printed
     current_time = time.time()
     delta_time = current_time - last_frame_time
     last_frame_time = current_time
-    
+
     # Reset camera position and FOV on backspace
-    if 'backspace' in active_keys:
+    if "backspace" in active_keys:
         camera_offset = torch.zeros(3, dtype=torch.float32)
         fov_offset = 0.0
 
     # Camera movement mapping based on active keys
-    if 'w' in active_keys:
+    if "w" in active_keys:
         camera_offset[0] -= MOVEMENT_SPEED * delta_time  # Move forward
-    if 's' in active_keys:
+    if "s" in active_keys:
         camera_offset[0] += MOVEMENT_SPEED * delta_time  # Move back
-    if 'd' in active_keys:
+    if "d" in active_keys:
         camera_offset[1] += MOVEMENT_SPEED * delta_time  # Move right
-    if 'a' in active_keys:
+    if "a" in active_keys:
         camera_offset[1] -= MOVEMENT_SPEED * delta_time  # Move left
-    if 'up' in active_keys:
+    if "up" in active_keys:
         camera_offset[2] += MOVEMENT_SPEED * delta_time  # Move up
-    if 'down' in active_keys:
+    if "down" in active_keys:
         camera_offset[2] -= MOVEMENT_SPEED * delta_time  # Move down
 
     # FOV control
-    if 'left' in active_keys:
+    if "left" in active_keys:
         fov_offset -= FOV_CHANGE_SPEED * delta_time
-    if 'right' in active_keys:
+    if "right" in active_keys:
         fov_offset += FOV_CHANGE_SPEED * delta_time
-    
+
     # update camera position and fov
     pos = sim_env.unwrapped.base_camera_settings["pos"] + camera_offset
     pose = sapien_utils.look_at(pos, sim_env.unwrapped.base_camera_settings["target"])
     sim_env.unwrapped.camera_mount.set_pose(pose)
-    sim_env.unwrapped._sensors["base_camera"].camera.set_fovy(sim_env.unwrapped.base_camera_settings["fov"] + fov_offset)
+    sim_env.unwrapped._sensors["base_camera"].camera.set_fovy(
+        sim_env.unwrapped.base_camera_settings["fov"] + fov_offset
+    )
 
     if len(active_keys) > 0:
         print("current_camera_position", pose.p)
-        print("current_camera_fov", sim_env.unwrapped.base_camera_settings["fov"] + fov_offset)
+        print(
+            "current_camera_fov",
+            sim_env.unwrapped.base_camera_settings["fov"] + fov_offset,
+        )
         help_message_printed = False  # Reset the flag when there's movement
-    elif not help_message_printed:  # Only print help message if it hasn't been printed yet
-        print("press: (w), (a) to move in x, (s), (d) to move in y, (up), (down) to move in z, (left), (right) to change fov")
+    elif (
+        not help_message_printed
+    ):  # Only print help message if it hasn't been printed yet
+        print(
+            "press: (w), (a) to move in x, (s), (d) to move in y, (up), (down) to move in z, (left), (right) to change fov"
+        )
         print("press: (backspace) to reset, close figure to exit")
         help_message_printed = True
 
@@ -149,22 +157,25 @@ if __name__ == "__main__":
     # max control freq for lerobot really is just 60Hz
     real_agent = LeRobotRealAgent(real_robot)
 
-
     max_episode_steps = 200
     sim_env = gym.make(
         "KochGraspCube-v1",
         obs_mode="rgb+segmentation",
         sim_config={"sim_freq": 120, "control_freq": 15},
-        render_mode="sensors", # only sensors mode is supported right now for real envs, basically rendering the direct visual observations fed to policy
-        max_episode_steps=max_episode_steps, # give our robot more time to try and re-try the task
+        render_mode="sensors",  # only sensors mode is supported right now for real envs, basically rendering the direct visual observations fed to policy
+        max_episode_steps=max_episode_steps,  # give our robot more time to try and re-try the task
         num_envs=1,
-        #domain_randomization=False,
+        # domain_randomization=False,
     )
     # you can apply most wrappers freely to the sim_env and the real env will use them
     sim_env = FlattenRGBDObservationWrapper(sim_env)
-    sim_env = RecordEpisode(sim_env, output_dir="videos", save_trajectory=False, video_fps=sim_env.unwrapped.control_freq)
+    sim_env = RecordEpisode(
+        sim_env,
+        output_dir="videos",
+        save_trajectory=False,
+        video_fps=sim_env.unwrapped.control_freq,
+    )
     sim_env.unwrapped.rgb_overlay_paths = None
-    
 
     real_env = Sim2RealEnv(sim_env=sim_env, agent=real_agent, obs_mode="rgb")
     sim_env.print_sim_details()
@@ -174,7 +185,7 @@ if __name__ == "__main__":
     # for plotting robot camera reads
     fig = plt.figure()
     ax = fig.add_subplot()
-    
+
     # Disable all default key bindings
     fig.canvas.mpl_disconnect(fig.canvas.manager.key_press_handler_id)
     fig.canvas.manager.key_press_handler_id = None
@@ -182,8 +193,8 @@ if __name__ == "__main__":
     # initialize the plot
     im = ax.imshow(overlay_envs(sim_env, real_env))
 
-    fig.canvas.mpl_connect('key_press_event', on_key_press)
-    fig.canvas.mpl_connect('key_release_event', on_key_release)
+    fig.canvas.mpl_connect("key_press_event", on_key_press)
+    fig.canvas.mpl_connect("key_release_event", on_key_release)
 
     if args.output_photo_path is not None:
         path, ext = args.output_photo_path.split(".")
