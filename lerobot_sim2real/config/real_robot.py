aa0b5aeffddd08ded26065cb828eda2ff0a88ed0
ysb <yuvibirdi27@gmail.com>
2025-09-29 18:28:34 -0400

    made the config changes need before the run

--- lerobot_sim2real/config/real_robot.py
+++ lerobot_sim2real/config/real_robot.py
@@ -5,6 +5,7 @@ from lerobot.common.robots.so100_follower.config_so100_follower import SO100Foll
 from lerobot.common.robots.utils import make_robot_from_config
 import numpy as np
 from lerobot.common.cameras.realsense.configuration_realsense import RealSenseCameraConfig
+from lerobot.common.cameras.opencv.configuration_opencv import OpenCVCameraConfig
 
 def create_real_robot(uid: str = "so100") -> Robot:
     """Wrapper function to map string UIDS to real robot configurations. Primarily for saving a bit of code for users when they fork the repository. They can just edit the camera, id etc. settings in this one file."""
@@ -13,13 +14,13 @@ def create_real_robot(uid: str = "so100") -> Robot:
             port="/dev/ttyACM0",
             use_degrees=True,
             # for phone camera users you can use the commented out setting below
-            # cameras={
-            #     "base_camera": OpenCVCameraConfig(camera_index=1, fps=30, width=640, height=480)
-            # }
-            # for intel realsense camera users you need to modify the serial number or name for your own hardware
             cameras={
-                "base_camera": RealSenseCameraConfig(serial_number_or_name="146322070293", fps=30, width=640, height=480)
+                "base_camera": OpenCVCameraConfig(index_or_path=0, fps=30, width=640, height=480)
             },
+            # for intel realsense camera users you need to modify the serial number or name for your own hardware
+            # cameras={
+            #     "base_camera": RealSenseCameraConfig(serial_number_or_name="146322070293", fps=30, width=640, height=480)
+            # },
             id="stone_home",
         )
         real_robot = make_robot_from_config(robot_config)
